a3e2bbab38097f3c3d0501f5c944e1e981e3c4c1
galoss88 <gallogarcianicolas@gmail.com>
2023-02-04 23:15:09 -0300

    a

--- client/src/routes/routesSadcom.jsx
+++ client/src/routes/routesSadcom.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import ListadoProductos from '../components/container/ListadoProductos/ListadoProductos';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import ListadoProductos from "../components/container/ListadoProductos/ListadoProductos";
 
 const routesSadcom = () => {
   return (
     <Routes>
-        <Route path='/productos' element={<ListadoProductos/>}>
-
-        </Route>
+      <Route path="/productos" element={<ListadoProductos />}></Route>
     </Routes>
-  )
-}
+  );
+};
 
 export default routesSadcom;

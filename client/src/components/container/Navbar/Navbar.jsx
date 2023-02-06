f58737080ef6b8fdb0a62d62657047df64d92a22
SophSawczuk <sofiasawczuka@gmail.com>
2023-02-06 14:07:25 -0300

    Navbar responsive

--- client/src/components/container/Navbar/Navbar.jsx
+++ client/src/components/container/Navbar/Navbar.jsx
@@ -5,26 +5,38 @@ import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import {FaShoppingCart} from "react-icons/fa"
+import styled from "styled-components";
 
 export default function NavBar() {
+    const Wrapper = styled.div`
+        display: flex;
+        @media (max-width: 768px) {
+            display: none;
+        }
+    `
     return(
         <div>
             
-                <Navbar key="xxl" bg="light" expand="xxl" className="mb-3">
+                <Navbar  key="xxl" bg="light" expand="xxl" className="mb-3">
                     <Container fluid>
+                    
                         <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-xxl`} />
-                        <b><Navbar.Brand href="/">Logo</Navbar.Brand></b>
-                        <Form className="d-flex">
-                            <Form.Control
-                                type="search"
-                                placeholder="Buscar..."
-                                className="me-2"
-                                aria-label="Search"
-                            />
-                            <Button variant="outline-secondary">Ir</Button>
-                        </Form>
-                        <Nav.Link><FaShoppingCart/> Carrito</Nav.Link>
-                        <Navbar.Offcanvas id={`offcanvasNavbar-expand-xxl`} aria-labelledby={`offcanvasNavbarLabel-expand-xxl`} placement="start">
+                        <b><Navbar.Brand href="/" /* style={{"padding-right": "25vw"}} */>Logo</Navbar.Brand></b>
+                        <Wrapper> 
+                            <Form className="d-flex"  style={{"padding-right": "30vw"}} >
+                                <Form.Control
+                                    type="search"
+                                    placeholder="Buscar..."
+                                    className="me-2"
+                                    aria-label="Search"
+                                />
+                                <Button variant="outline-secondary">Ir</Button>
+                            </Form>
+                            <Nav.Link style={{"padding-top": "1vh"}}><FaShoppingCart/> Carrito</Nav.Link>
+                            </Wrapper> 
+                     
+                   
+                        <Navbar.Offcanvas id={`offcanvasNavbar-expand-xxl `} aria-labelledby={`offcanvasNavbarLabel-expand-xxl`} placement="start">
                             
                             <Offcanvas.Body>
                                 <Nav className="flex-grow-2 pe-3 align-items-center">
@@ -33,14 +45,16 @@ export default function NavBar() {
                                     <hr/>
                                     <Nav.Link href="#action2">Categorias</Nav.Link>
                                     <hr/>
+                                    <Nav.Link href="#action2">Carrito</Nav.Link>
+                                    <hr/>
                                     <Nav.Link href="#action2">Preguntas frecuentes</Nav.Link>
                                     <hr/>
                                     <Nav.Link href="#action2">Contacto</Nav.Link>
-                                    <hr/>
                                 </Nav>
                             </Offcanvas.Body>
                             
                         </Navbar.Offcanvas>
+                        
                     </Container>
                 </Navbar>
           

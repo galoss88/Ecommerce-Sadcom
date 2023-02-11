1ea6482021f9bc07e33c0af48f7f49e83cbf85f6
LuCardozo <lucianocar12300@gmail.com>
2023-02-11 02:46:01 -0300

    arreglos del back

--- api/src/db.js
+++ api/src/db.js
@@ -1,22 +1,24 @@
+require("dotenv").config();
+const {user, password, server, database} = process.env;
 const mssql = require("mssql");
-
 const dbSettings = {
-    user: "sadcom",
-    password: "com2022",
-    server: "localhost",
-    database: "sadcomEcommerce",
+    user: user,
+    password: password,
+    server: server,
+    database: database,
     options: {
             encrypt: true, // for azure
             trustServerCertificate: true // change to true for local dev / self-signed certs
     }
 };
-
 const getConection = async () =>{
     try {
         const pool = await mssql.connect(dbSettings);
+        console.log(user)
         return pool;
     } catch (error) {
         console.log(error);
     }
 };
-module.exports = getConection();
+getConection();
+module.exports = {getConection};
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
-
 const router = Router();
-
+const { getConection } = require("../db")
 router.get("/", async (req, res)=>{
-    res.send("Estamos listos");
+    const pool = await getConection();
+    const result = await pool.request().query("SELECT 1")
+    res.status(201).send(result);
 });
 
 module.exports = router;

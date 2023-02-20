05e60b07b7a143bbf17e529949f53cc960035334
galoss88 <gallogarcianicolas@gmail.com>
2023-02-20 18:58:34 -0300

    agregado sequelize

--- api/src/db.js
+++ api/src/db.js
@@ -1,24 +1,45 @@
-require("dotenv").config();
-const {user, password, server, database} = process.env;
-const mssql = require("mssql");
-const dbSettings = {
-    user: user,
-    password: password,
-    server: server,
-    database: database,
-    options: {
-            encrypt: true, // for azure
-            trustServerCertificate: true // change to true for local dev / self-signed certs
-    }
-};
-const getConection = async () =>{
-    try {
-        const pool = await mssql.connect(dbSettings);
-        console.log(user)
-        return pool;
-    } catch (error) {
-        console.log(error);
-    }
-};
-getConection();
-module.exports = {getConection};
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("SadcomEcommerce", "galo88", "78914526", {
+  dialect: "mssql",
+  host: "localhost",
+  port: 1433, //predeterminado de SQL server
+  dialectOptions: {
+    optiones: {
+      encrypt: true,
+    },
+  },
+});
+//Comprobar si funciona sequelize
+sequelize.authenticate()
+  .then(() => {
+      console.log('Conexión establecida correctamente.');
+  })
+  .catch(err => {
+      console.error('Error al conectarse a la base de datos:', err);
+  });
+
+// require("dotenv").config();
+// const { user, password, server, database } = process.env;
+// const mssql = require("mssql");
+// const dbSettings = {
+//   user,
+//   password,
+//   server,
+//   database,
+//   options: {
+//     encrypt: true, // for azure
+//     trustServerCertificate: true, // change to true for local dev / self-signed certs
+//   },
+// };
+
+// const getConection = async () => {
+//   try {
+//     const pool = await mssql.connect(dbSettings);
+//     const result = pool.request().query("SELECT 1");
+//     return pool;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+// getConection();
+// module.exports = { getConection };

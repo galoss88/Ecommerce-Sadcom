a23d82af9069ed452aa39e4853f38f73c0c0e54e
galoss88 <gallogarcianicolas@gmail.com>
2023-02-27 20:34:50 -0300

    a

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,24 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 //Bootstrap
 import "bootstrap/dist/css/bootstrap.css";
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
 //CSS index
-import './index.css'
+import "./index.css";
 //React Router
 import { BrowserRouter } from "react-router-dom";
-
+//AUTH0
+import { Auth0Provider } from "@auth0/auth0-react";
 //redux
 import { Provider } from "react-redux";
-import store from "./redux/store/index"
+import store from "./redux/store/index";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-  <BrowserRouter>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </BrowserRouter>,
+    <BrowserRouter>
+      <React.StrictMode>
+        <Auth0Provider
+          domain="dev-5gwtap86i6z84pxw.us.auth0.com"
+          clientId="xSjFjns2wmPNyZsyn9ePZmh23tGP35eL"
+          authorizationParams={{
+            redirect_uri: window.location.origin,
+          }}
+        >
+          <App />
+        </Auth0Provider>
+      </React.StrictMode>
+    </BrowserRouter>
+    ,
   </Provider>
-)
+);

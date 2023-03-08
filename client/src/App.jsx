03360ca77998bd8549d46a5efcd24b3439375aa6
galoss88 <gallogarcianicolas@gmail.com>
2023-03-08 18:50:04 -0300

    a

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import ListaProductosProvider from "./context/ContextListaProductos";
 import ContextLoginProvider from "./context/ContextLogin";
 import RoutesSadcom from "./routes/routesSadcom";
+import { enviarSocket } from "./utils/enviarSocket";
 
 function App() {
   return (

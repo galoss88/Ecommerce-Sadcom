1c09b9b4be9868c807398dc4d339e5550fee5177
SophSawczuk <sofiasawczuka@gmail.com>
2023-03-08 20:27:57 -0300

    navbar responsive productos

--- client/src/components/container/Navbar/styles/stylesNavbar.jsx
+++ client/src/components/container/Navbar/styles/stylesNavbar.jsx
@@ -6,13 +6,13 @@ position: relative;
 
 align-items: center;
 @media (max-width: 768px) {
-  display: none;
+  /* display: none; */
 }
 `;
 
 export const WrapperResponsive = styled.div`
-  display: flex;
-  @media (max-width: 768px) {
+   display: flex;
+   @media (max-width: 768px) {
     display: none;
   }
 `
